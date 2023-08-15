0f7ad72783be9f64c5d0827fe0a13ad34e20ee97
Jonathan Yang <jyang27@iris-ws-1.stanford.edu>
2023-08-15 13:59:42 -0700

    Add depndency fixes

--- R2D2/R2D2_dataset_builder.py
+++ R2D2/R2D2_dataset_builder.py
@@ -5,8 +5,393 @@ import numpy as np
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import tensorflow_hub as hub
-from r2d2.trajectory_utils.misc import load_trajectory
-from r2d2.data_loading.trajectory_sampler import crawler
+import os
+import h5py
+import cv2
+import json
+from collections import defaultdict
+import random
+from copy import deepcopy
+
+camera_type_dict = {
+    'hand_camera_id': 0,
+    'varied_camera_1_id': 1,
+    'varied_camera_2_id': 1,
+}
+
+camera_type_to_string_dict = {
+    0: "hand_camera",
+    1: "varied_camera",
+    2: "fixed_camera",
+}
+
+
+def get_camera_type(cam_id):
+    if cam_id not in camera_type_dict:
+        return None
+    type_int = camera_type_dict[cam_id]
+    type_str = camera_type_to_string_dict[type_int]
+    return type_str
+
+
+class MP4Reader:
+    def __init__(self, filepath, serial_number):
+        # Save Parameters #
+        self.serial_number = serial_number
+        self._index = 0
+
+        # Open Video Reader #
+        self._mp4_reader = cv2.VideoCapture(filepath)
+        if not self._mp4_reader.isOpened():
+            print(filepath)
+            raise RuntimeError("Corrupted MP4 File")
+
+        # Load Recording Timestamps #
+        timestamp_filepath = filepath[:-4] + "_timestamps.json"
+        if not os.path.isfile(timestamp_filepath):
+            self._recording_timestamps = []
+        with open(timestamp_filepath, "r") as jsonFile:
+            self._recording_timestamps = json.load(jsonFile)
+
+    def set_reading_parameters(
+        self,
+        image=True,
+        concatenate_images=False,
+        resolution=(0, 0),
+        resize_func=None,
+    ):
+        # Save Parameters #
+        self.image = image
+        self.concatenate_images = concatenate_images
+        self.resolution = resolution
+        self.resize_func = cv2.resize
+        self.skip_reading = not image
+        if self.skip_reading:
+            return
+
+    def get_frame_resolution(self):
+        width = self._mp4_reader.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)
+        height = self._mp4_reader.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)
+        return (width, height)
+
+    def get_frame_count(self):
+        if self.skip_reading:
+            return 0
+        frame_count = int(self._mp4_reader.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
+        return frame_count
+
+    def set_frame_index(self, index):
+        if self.skip_reading:
+            return
+
+        if index < self._index:
+            self._mp4_reader.set(cv2.CAP_PROP_POS_FRAMES, index - 1)
+            self._index = index
+
+        while self._index < index:
+            self.read_camera(ignore_data=True)
+
+    def _process_frame(self, frame):
+        frame = deepcopy(frame)
+        if self.resolution == (0, 0):
+            return frame
+        return self.resize_func(frame, self.resolution)
+        # return cv2.resize(frame, self.resolution)#, interpolation=cv2.INTER_AREA)
+
+    def read_camera(self, ignore_data=False, correct_timestamp=None, return_timestamp=False):
+        # Skip if Read Unnecesary #
+        if self.skip_reading:
+            return {}
+
+        # Read Camera #
+        success, frame = self._mp4_reader.read()
+        try:
+            received_time = self._recording_timestamps[self._index]
+        except IndexError:
+            received_time = None
+
+        self._index += 1
+        if not success:
+            return None
+        if ignore_data:
+            return None
+
+        # Check Image Timestamp #
+        timestamps_given = (received_time is not None) and (correct_timestamp is not None)
+        if timestamps_given and (correct_timestamp != received_time):
+            print("Timestamps did not match...")
+            return None
+
+        # Return Data #
+        data_dict = {}
+
+        if self.concatenate_images:
+            data_dict["image"] = {self.serial_number: self._process_frame(frame)}
+        else:
+            single_width = frame.shape[1] // 2
+            data_dict["image"] = {
+                self.serial_number + "_left": self._process_frame(frame[:, :single_width, :]),
+                self.serial_number + "_right": self._process_frame(frame[:, single_width:, :]),
+            }
+
+        if return_timestamp:
+            return data_dict, received_time
+        return data_dict
+
+    def disable_camera(self):
+        if hasattr(self, "_mp4_reader"):
+            self._mp4_reader.release()
+
+
+class RecordedMultiCameraWrapper:
+    def __init__(self, recording_folderpath, camera_kwargs={}):
+        # Save Camera Info #
+        self.camera_kwargs = camera_kwargs
+
+        # Open Camera Readers #
+        svo_filepaths = glob.glob(recording_folderpath + "/*.svo")
+        mp4_filepaths = glob.glob(recording_folderpath + "/*.mp4")
+        all_filepaths = svo_filepaths + mp4_filepaths
+
+        self.camera_dict = {}
+        for f in all_filepaths:
+            serial_number = f.split("/")[-1][:-4]
+            cam_type = get_camera_type(serial_number)
+            camera_kwargs.get(cam_type, {})
+
+            if f.endswith(".svo"):
+                Reader = SVOReader
+            elif f.endswith(".mp4"):
+                Reader = MP4Reader
+            else:
+                raise ValueError
+
+            self.camera_dict[serial_number] = Reader(f, serial_number)
+
+    def read_cameras(self, index=None, camera_type_dict={}, timestamp_dict={}):
+        full_obs_dict = defaultdict(dict)
+
+        # Read Cameras In Randomized Order #
+        all_cam_ids = list(self.camera_dict.keys())
+        #random.shuffle(all_cam_ids)
+
+        for cam_id in all_cam_ids:
+            cam_type = camera_type_dict[cam_id]
+            curr_cam_kwargs = self.camera_kwargs.get(cam_type, {})
+            self.camera_dict[cam_id].set_reading_parameters(**curr_cam_kwargs)
+
+            timestamp = timestamp_dict.get(cam_id + "_frame_received", None)
+            if index is not None:
+                self.camera_dict[cam_id].set_frame_index(index)
+
+            data_dict = self.camera_dict[cam_id].read_camera(correct_timestamp=timestamp)
+
+            # Process Returned Data #
+            if data_dict is None:
+                return None
+            for key in data_dict:
+                full_obs_dict[key].update(data_dict[key])
+
+        return full_obs_dict
+
+
+
+def get_hdf5_length(hdf5_file, keys_to_ignore=[]):
+    length = None
+
+    for key in hdf5_file.keys():
+        if key in keys_to_ignore:
+            continue
+
+        curr_data = hdf5_file[key]
+        if isinstance(curr_data, h5py.Group):
+            curr_length = get_hdf5_length(curr_data, keys_to_ignore=keys_to_ignore)
+        elif isinstance(curr_data, h5py.Dataset):
+            curr_length = len(curr_data)
+        else:
+            raise ValueError
+
+        if length is None:
+            length = curr_length
+        assert curr_length == length
+
+    return length
+
+
+def load_hdf5_to_dict(hdf5_file, index, keys_to_ignore=[]):
+    data_dict = {}
+
+    for key in hdf5_file.keys():
+        if key in keys_to_ignore:
+            continue
+
+        curr_data = hdf5_file[key]
+        if isinstance(curr_data, h5py.Group):
+            data_dict[key] = load_hdf5_to_dict(curr_data, index, keys_to_ignore=keys_to_ignore)
+        elif isinstance(curr_data, h5py.Dataset):
+            data_dict[key] = curr_data[index]
+        else:
+            raise ValueError
+
+    return data_dict
+
+
+
+class TrajectoryReader:
+    def __init__(self, filepath, read_images=True):
+        self._hdf5_file = h5py.File(filepath, "r")
+        is_video_folder = "observations/videos" in self._hdf5_file
+        self._read_images = read_images and is_video_folder
+        self._length = get_hdf5_length(self._hdf5_file)
+        self._video_readers = {}
+        self._index = 0
+
+    def length(self):
+        return self._length
+
+    def read_timestep(self, index=None, keys_to_ignore=[]):
+        # Make Sure We Read Within Range #
+        if index is None:
+            index = self._index
+        else:
+            assert not self._read_images
+            self._index = index
+        assert index < self._length
+
+        # Load Low Dimensional Data #
+        keys_to_ignore = [*keys_to_ignore.copy(), "videos"]
+        timestep = load_hdf5_to_dict(self._hdf5_file, self._index, keys_to_ignore=keys_to_ignore)
+
+        # Load High Dimensional Data #
+        if self._read_images:
+            camera_obs = self._uncompress_images()
+            timestep["observations"]["image"] = camera_obs
+
+        # Increment Read Index #
+        self._index += 1
+
+        # Return Timestep #
+        return timestep
+
+    def _uncompress_images(self):
+        # WARNING: THIS FUNCTION HAS NOT BEEN TESTED. UNDEFINED BEHAVIOR FOR FAILED READING. #
+        video_folder = self._hdf5_file["observations/videos"]
+        camera_obs = {}
+
+        for video_id in video_folder:
+            # Create Video Reader If One Hasn't Been Made #
+            if video_id not in self._video_readers:
+                serialized_video = video_folder[video_id]
+                filename = create_video_file(byte_contents=serialized_video)
+                self._video_readers[video_id] = imageio.get_reader(filename)
+
+            # Read Next Frame #
+            camera_obs[video_id] = yield self._video_readers[video_id]
+            # Future Note: Could Make Thread For Each Image Reader
+
+        # Return Camera Observation #
+        return camera_obs
+
+    def close(self):
+        self._hdf5_file.close()
+
+
+def crawler(dirname, filter_func=None):
+    subfolders = [f.path for f in os.scandir(dirname) if f.is_dir()]
+    traj_files = [f.path for f in os.scandir(dirname) if (f.is_file() and "trajectory.h5" in f.path)]
+
+    if len(traj_files):
+        # Only Save Desired Data #
+        if filter_func is None:
+            use_data = True
+        else:
+            hdf5_file = h5py.File(traj_files[0], "r")
+            use_data = filter_func(hdf5_file.attrs)
+            hdf5_file.close()
+
+        if use_data:
+            return [dirname]
+
+    all_folderpaths = []
+    for child_dirname in subfolders:
+        child_paths = crawler(child_dirname, filter_func=filter_func)
+        all_folderpaths.extend(child_paths)
+
+    return all_folderpaths
+
+
+def load_trajectory(
+    filepath=None,
+    read_cameras=True,
+    recording_folderpath=None,
+    camera_kwargs={},
+    remove_skipped_steps=False,
+    num_samples_per_traj=None,
+    num_samples_per_traj_coeff=1.5,
+):
+    read_hdf5_images = read_cameras and (recording_folderpath is None)
+    read_recording_folderpath = read_cameras and (recording_folderpath is not None)
+
+    traj_reader = TrajectoryReader(filepath, read_images=read_hdf5_images)
+    if read_recording_folderpath:
+        camera_reader = RecordedMultiCameraWrapper(recording_folderpath, camera_kwargs)
+
+    horizon = traj_reader.length()
+    timestep_list = []
+
+    # Choose Timesteps To Save #
+    if num_samples_per_traj:
+        num_to_save = num_samples_per_traj
+        if remove_skipped_steps:
+            num_to_save = int(num_to_save * num_samples_per_traj_coeff)
+        max_size = min(num_to_save, horizon)
+        indices_to_save = np.sort(np.random.choice(horizon, size=max_size, replace=False))
+    else:
+        indices_to_save = np.arange(horizon)
+
+    # Iterate Over Trajectory #
+    for i in indices_to_save:
+        # Get HDF5 Data #
+        timestep = traj_reader.read_timestep(index=i)
+
+        # If Applicable, Get Recorded Data #
+        if read_recording_folderpath:
+            timestamp_dict = timestep["observation"]["timestamp"]["cameras"]
+            camera_type_dict = {
+                k: camera_type_to_string_dict[v] for k, v in timestep["observation"]["camera_type"].items()
+            }
+            camera_obs = camera_reader.read_cameras(
+                index=i, camera_type_dict=camera_type_dict, timestamp_dict=timestamp_dict
+            )
+            camera_failed = camera_obs is None
+
+            # Add Data To Timestep If Successful #
+            if camera_failed:
+                break
+            else:
+                timestep["observation"].update(camera_obs)
+        
+        # Filter Steps #
+        step_skipped = not timestep["observation"]["controller_info"].get("movement_enabled", True)
+        delete_skipped_step = step_skipped and remove_skipped_steps
+
+        # Save Filtered Timesteps #
+        if delete_skipped_step:
+            del timestep
+        else:
+            timestep_list.append(timestep)
+
+    # Remove Extra Transitions #
+    timestep_list = np.array(timestep_list)
+    if (num_samples_per_traj is not None) and (len(timestep_list) > num_samples_per_traj):
+        ind_to_keep = np.random.choice(len(timestep_list), size=num_samples_per_traj, replace=False)
+        timestep_list = timestep_list[ind_to_keep]
+
+    # Close Readers #
+    traj_reader.close()
+
+    # Return Data #
+    return timestep_list
 
 
 class R2D2(tfds.core.GeneratorBasedBuilder):
@@ -25,7 +410,7 @@ class R2D2(tfds.core.GeneratorBasedBuilder):
         """Dataset metadata (homepage, citation,...)."""
         return self.dataset_info_from_configs(
             features=tfds.features.FeaturesDict({
-                'steps': tfds.features.Dataset({
+            'steps': tfds.features.Dataset({
                     'observation': tfds.features.FeaturesDict({
                         'exterior_image_1_left': tfds.features.Image(
                             shape=(256, 256, 3),
@@ -59,7 +444,7 @@ class R2D2(tfds.core.GeneratorBasedBuilder):
                         ),
                         'wrist_image_right': tfds.features.Image(
                             shape=(256, 256, 3),
-                            dytype=np.uint8,
+                            dtype=np.uint8,
                             encoding_format='jpeg',
                             doc='Wrist camera RGB right viewpoint'
                         ),
@@ -105,17 +490,17 @@ class R2D2(tfds.core.GeneratorBasedBuilder):
                             dtype=np.float32,
                             doc='Commanded joint position'
                         ),
-                        'joint_velocity': tdfs features.Tensor(
-                            shape=(7,)
+                        'joint_velocity': tfds.features.Tensor(
+                            shape=(7,),
                             dtype=np.float32,
                             doc='Commanded joint velocity'
                         )
                     }),
-                    tfds.features.Tensor(
+                    'action': tfds.features.Tensor(
                         shape=(10,),
                         dtype=np.float32,
-                        doc='Robot action, consists of [7x joint velocities, '
-                            '2x gripper velocities, 1x terminate episode].',
+                        doc='Robot action, consists of [7x joint velocities, \
+                            2x gripper velocities, 1x terminate episode].',
                     ),
                     'discount': tfds.features.Scalar(
                         dtype=np.float32,
@@ -157,15 +542,9 @@ class R2D2(tfds.core.GeneratorBasedBuilder):
     def _split_generators(self, dl_manager: tfds.download.DownloadManager):
         """Define data splits."""
         return {
-            'train': self._generate_examples(path='data/train/episode_*.npy'),
-            'val': self._generate_examples(path='data/val/episode_*.npy'),
+            'train': self._generate_examples(path='/iris/u/jyang27/2023-02-28'),
+            #'val': self._generate_examples(''),
         }
-
-    # Remove Extra Transitions #
-    timestep_list = np.array(timestep_list)
-    if (num_samples_per_traj is not None) and (len(timestep_list) > num_samples_per_traj):
-        ind_to_keep = np.random.choice(len(timestep_list), size=num_samples_per_traj, replace=False)
-        timestep_list = timestep_list[ind_to_keep]
 
 
     def _generate_examples(self, path) -> Iterator[Tuple[str, Any]]:
@@ -175,15 +554,26 @@ class R2D2(tfds.core.GeneratorBasedBuilder):
             return tf.io.encode_jpeg(tf.cat(tf.round(tf.image.resize(image, size, method="bicubic")),
                 tf.uint8))
 
+        def flatten(x):
+            d = {}
+            for k, v in x.items():
+                if isinstance(v, dict):
+                    for k2, v2 in flatten(v).items():
+                        d[k + "/" + k2] = v2
+                else:
+                    d[k] = v
+            return d
+
+
         def _parse_example(episode_path):
             FRAMESKIP = 1
             IMAGE_SIZE = (256, 256)
 
-            h5_filepath = os.path.join(path, 'trajectory.h5')
-            recording_folderpath = os.path.join(path, 'recordings', 'MP4')
-            
+            h5_filepath = os.path.join(episode_path, 'trajectory.h5')
+            recording_folderpath = os.path.join(episode_path, 'recordings', 'MP4')
+                
             traj = load_trajectory(h5_filepath, recording_folderpath=recording_folderpath)
-            traj = traj[::frameskip]
+            traj = traj[::FRAMESKIP]
 
             data = [flatten(t) for t in traj]
             assert all(t.keys() == traj_flat[0].keys() for t in data)
@@ -201,12 +591,11 @@ class R2D2(tfds.core.GeneratorBasedBuilder):
                 language_instruction = 'Execute a task.'
                 # compute Kona language embedding
                 language_embedding = self._embed([language_instruction])[0].numpy()
-
                 episode.append({
                     'observation': {
                         'exterior_image_1_left': obs['image']['29838012_left'],
                         'exterior_image_1_right': obs['image']['29838012_right'],
-                        'exterior_image_2_left': obs['image']['23404442_left']
+                        'exterior_image_2_left': obs['image']['23404442_left'],
                         'exterior_image_2_right': obs['image']['23404442_right'],
                         'wrist_image_left': obs['image']['19824535_left'],
                         'wrist_image_right': obs['image']['19824535_right'],
@@ -248,15 +637,16 @@ class R2D2(tfds.core.GeneratorBasedBuilder):
         episode_paths = [p for p in episode_paths if os.path.exists(p + '/trajectory.h5') and \
                 os.path.exists(p + '/recordings/MP4')]
 
+        print(episode_paths)
         # for smallish datasets, use single-thread parsing
-        #for sample in episode_paths:
-        #    yield _parse_example(sample)
+        for sample in episode_paths:
+            yield _parse_example(sample)
 
         # for large datasets use beam to parallelize data parsing (this will have initialization overhead)
-        beam = tfds.core.lazy_imports.apache_beam
-        return (
-                 beam.Create(episode_paths)
-                 | beam.Map(_parse_example)
-        )
+        #beam = tfds.core.lazy_imports.apache_beam
+        #return (
+        #         beam.Create(episode_paths)
+        #         | beam.Map(_parse_example)
+        #)
 
 
